d1583abcb0f797f6036ffbe2a90d78b033ff5221
Tahsin Rahman <tr174@duke.edu>
2025-01-24 21:28:22 -0500

    finished printhelp function

--- filecrypt/filecrypt.go
+++ filecrypt/filecrypt.go
@@ -1,13 +1,14 @@
 package main
 
+import "fmt"
 
-func main(){
-	if len (os.Args) < 2{
+func main() {
+	if len(os.Args) < 2 {
 		printHelp()
 		os.Exit(0)
 	}
 	function := os.Args[1]
-	switch function{
+	switch function {
 	case "help":
 		printHelp()
 	case "encrypt":
@@ -20,28 +21,39 @@ func main(){
 	}
 }
 
-
-func printHelp(){
-
-}
-
-
-func encryptHandle(){
-
-}
-
-func decryptHandle(){
-
-}
-
-func getPassword(){
-
-}
-
-func validatePassword(){
+func printHelp() {
+	fmt.Println("File encryption")
+	fmt.Println("Simple file encrypter for your day to day needs")
+	fmt.Println("")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("\tgo run . encrypt /path/to/your/file")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("")
+	fmt.Println("\t encrypt \tEncrypts a file given a password")
+	fmt.Println("\t decrypt \tTries to decrypt a file using a password")
+	fmt.Println("\t help\t\tDisplays help text")
+	fmt.Println("")
 
 }
 
-func validateFile(){
-	
+func encryptHandle() {
+
+}
+
+func decryptHandle() {
+
+}
+
+func getPassword() {
+
+}
+
+func validatePassword() {
+
+}
+
+func validateFile() {
+
 }
